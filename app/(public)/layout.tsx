a9b88c9ff9414accf2882e17d3de64b3cdb33bc0
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-23 23:12:08 -0400

    changes from dynamic component to tailwind component

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,12 +1,5 @@
 import { SiteFooter } from "@/components/footer"
 import { SiteHeader } from "@/components/header/site-header"
-import dynamic from 'next/dynamic';
-
-
-const DynamicSiteHeader = dynamic(() => import('@/components/header/site-header'), {
-  loading: () => <p>Loading Header...</p>, // Placeholder component while loading
-  ssr: false, // Disable server-side rendering
-});
 
 export default function PublicLayout({
   children,
@@ -16,7 +9,7 @@ export default function PublicLayout({
   return (
     <>
       <div className="relative flex min-h-screen flex-col">
-        <DynamicSiteHeader />
+        <SiteHeader />
         <div className="flex-1">{children}</div>
         <SiteFooter />
       </div>
--- components/header/site-header.tsx
+++ components/header/site-header.tsx
@@ -7,20 +7,11 @@ import { ThemeToggle } from "@/components/theme-toggle"
 import { AvatarWithMenu } from "./avatar-with-menu"
 import { LanguageSelector } from "./language"
 import { MobileMenu } from "./mobile-menu"
-import { useTheme } from "next-themes"
-
 
 export function SiteHeader() {
-  const { theme } = useTheme();
-
-
-  const darkClass = 'border-gardient-dark';
-  const lightClass = 'border-gardient-light';
-
-  const headerClass = theme === 'dark' ? darkClass : lightClass;
 
   return (
-    <header className={`sticky top-0 z-40 w-full bg-background ${headerClass}`}>
+    <header className={`sticky top-0 z-40 w-full bg-background border-gardient-h`}>
       <div className="flex h-20 items-center space-x-4 px-4 sm:justify-between sm:space-x-0 md:px-8">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
@@ -36,4 +27,4 @@ export function SiteHeader() {
   )
 }
 
-export default SiteHeader;
+export default SiteHeader
