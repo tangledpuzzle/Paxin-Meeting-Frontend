144898c29cec8d57218601d2dd6424f27d5fa612
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-25 13:36:18 -0400

    fix metadata

--- lib/dynamicMetadata.tsx
+++ lib/dynamicMetadata.tsx
@@ -7,7 +7,7 @@ export function MetadataUpdater() {
   const { theme } = useTheme();
 
   const updateFavicon = (theme: string) => {
-    const faviconLink = document.querySelector("link[rel~='icon']");
+    const faviconLink = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
     if (!faviconLink) return;
 
     if (theme === 'light') {
@@ -18,7 +18,7 @@ export function MetadataUpdater() {
   };
 
   const updateManifest = (theme: string) => {
-    const manifestLink = document.querySelector("link[rel~='manifest']");
+    const manifestLink = document.querySelector("link[rel~='manifest']") as HTMLLinkElement;
     if (!manifestLink) return;
 
     if (theme === 'dark') {
