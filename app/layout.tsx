70b907f373c07702008de84995c2208a934643a0
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-25 13:26:00 +0800

    feat: PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import App from "@/provider/provider"
+import App from '@/provider/provider';
 
-import "@/styles/globals.css"
+import '@/styles/globals.css';
 
-import { Metadata, Viewport } from "next"
+import { Metadata, Viewport } from 'next';
 
-import { siteConfig } from "@/config/site"
-import { fontDMSans, fontRoboto, fontSans, fontSatoshi } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
+import { siteConfig } from '@/config/site';
+import { fontDMSans, fontRoboto, fontSans, fontSatoshi } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
+import { TailwindIndicator } from '@/components/tailwind-indicator';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
   title: {
@@ -16,32 +16,32 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  manifest: '/manifest.webmanifest',
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: '/favicon.ico',
+    shortcut: '/icons/favicon-16x16.png',
   },
-}
+};
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
-}
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <App>
-      <html lang="en" suppressHydrationWarning>
+      <html lang='en' suppressHydrationWarning>
         <head />
         <body
           className={cn(
-            "min-h-screen bg-background font-satoshi antialiased",
+            'min-h-screen bg-background font-satoshi antialiased',
             fontSans.variable,
             fontSatoshi.variable,
             fontDMSans.variable,
@@ -49,8 +49,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
           )}
         >
           <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
+            attribute='class'
+            defaultTheme='system'
             enableSystem={true}
           >
             {children}
@@ -59,5 +59,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </body>
       </html>
     </App>
-  )
+  );
 }
