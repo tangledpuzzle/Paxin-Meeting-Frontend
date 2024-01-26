22104e0947175c7aae3a5f76d3e139231d832c4c
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-26 23:25:11 +0800

    fix: remove slick carousel

--- components/home/profile/profile-card-skeleton.tsx
+++ components/home/profile/profile-card-skeleton.tsx
@@ -1,39 +1,36 @@
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from '@/components/ui/card';
 
-import "slick-carousel/slick/slick-theme.css"
-import "slick-carousel/slick/slick.css"
-
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from '@/components/ui/skeleton';
 
 function ProfileCardSkeleton() {
   return (
-    <Card className="size-full max-w-[320px] sm:max-w-[400px]">
-      <CardContent className="relative flex size-full flex-col gap-4 p-3">
-        <div className="relative">
-          <Skeleton className="h-[300px] w-full" />
+    <Card className='size-full max-w-[320px] sm:max-w-[400px]'>
+      <CardContent className='relative flex size-full flex-col gap-4 p-3'>
+        <div className='relative'>
+          <Skeleton className='h-[300px] w-full' />
         </div>
-        <div className="relative w-full max-w-[100%]">
-          <Skeleton className="h-6 w-full" />
+        <div className='relative w-full max-w-[100%]'>
+          <Skeleton className='h-6 w-full' />
         </div>
-        <div className="relative">
-          <Skeleton className="absolute right-0 top-3 size-8 rounded-full" />
+        <div className='relative'>
+          <Skeleton className='absolute right-0 top-3 size-8 rounded-full' />
         </div>
-        <div className="space-y-1">
-          <Skeleton className="h-5 w-20" />
-          <Skeleton className="h-5 w-2/3" />
+        <div className='space-y-1'>
+          <Skeleton className='h-5 w-20' />
+          <Skeleton className='h-5 w-2/3' />
         </div>
-        <div className="mt-auto space-y-1">
-          <Skeleton className="h-5 w-4/5" />
-          <Skeleton className="h-5 w-4/5" />
+        <div className='mt-auto space-y-1'>
+          <Skeleton className='h-5 w-4/5' />
+          <Skeleton className='h-5 w-4/5' />
         </div>
-        <div className="flex gap-3">
-          <Skeleton className="h-20 w-1/2" />
-          <Skeleton className="h-20 w-1/2" />
+        <div className='flex gap-3'>
+          <Skeleton className='h-20 w-1/2' />
+          <Skeleton className='h-20 w-1/2' />
         </div>
-        <Skeleton className="h-8 w-full" />
+        <Skeleton className='h-8 w-full' />
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export { ProfileCardSkeleton }
+export { ProfileCardSkeleton };
--- components/home/profile/profile-card.tsx
+++ components/home/profile/profile-card.tsx
@@ -1,44 +1,41 @@
-import Image from "next/image"
-import { BiSolidCalendar } from "react-icons/bi"
-import { BsCalendarDateFill } from "react-icons/bs"
-import { GrArticle } from "react-icons/gr"
+import Image from 'next/image';
+import { BiSolidCalendar } from 'react-icons/bi';
+import { BsCalendarDateFill } from 'react-icons/bs';
+import { GrArticle } from 'react-icons/gr';
 
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { TagSlider } from "@/components/common/tag-slider"
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { TagSlider } from '@/components/common/tag-slider';
 
-import "slick-carousel/slick/slick-theme.css"
-import "slick-carousel/slick/slick.css"
+import Link from 'next/link';
 
-import Link from "next/link"
-
-import { QRCodeModal } from "../../common/qrcode-modal"
-import { CategoryCard } from "./category-card"
-import { CityCard } from "./city-card"
+import { QRCodeModal } from '../../common/qrcode-modal';
+import { CategoryCard } from './category-card';
+import { CityCard } from './city-card';
 
 export interface ProfileCardProps {
-  username: string
-  bio: string
-  avatar: string
-  tags: string[]
-  cities: string[]
-  categories: string[]
-  qrcode: string
-  countrycode: string
+  username: string;
+  bio: string;
+  avatar: string;
+  tags: string[];
+  cities: string[];
+  categories: string[];
+  qrcode: string;
+  countrycode: string;
   review: {
     totaltime: {
-      hour: number
-      minutes: number
-      seconds: number
-    }
+      hour: number;
+      minutes: number;
+      seconds: number;
+    };
     monthtime: {
-      hour: number
-      minutes: number
-      seconds: number
-    }
-    totalposts: number
-  }
+      hour: number;
+      minutes: number;
+      seconds: number;
+    };
+    totalposts: number;
+  };
 }
 
 function ProfileCard(profile: ProfileCardProps) {
@@ -52,75 +49,75 @@ function ProfileCard(profile: ProfileCardProps) {
     qrcode,
     countrycode,
     review,
-  } = profile
+  } = profile;
 
   return (
-    <Card className="size-full max-w-[320px] sm:max-w-[400px]">
-      <CardContent className="relative flex size-full flex-col gap-4 p-3">
-        <div className="relative">
-          <div className="h-[300px] w-full">
+    <Card className='size-full max-w-[320px] sm:max-w-[400px]'>
+      <CardContent className='relative flex size-full flex-col gap-4 p-3'>
+        <div className='relative'>
+          <div className='h-[300px] w-full'>
             <Image
               src={avatar}
-              layout="fill"
-              objectFit="contain"
-              objectPosition="center"
-              alt="profile"
+              layout='fill'
+              objectFit='contain'
+              objectPosition='center'
+              alt='profile'
             />
           </div>
-          <div className="absolute inset-0 flex items-center justify-center rounded-lg rounded-b-none bg-gradient-to-b from-transparent via-transparent to-white dark:to-black"></div>
-          <div className="absolute top-3 flex w-full justify-between gap-2 px-4">
+          <div className='absolute inset-0 flex items-center justify-center rounded-lg rounded-b-none bg-gradient-to-b from-transparent via-transparent to-white dark:to-black'></div>
+          <div className='absolute top-3 flex w-full justify-between gap-2 px-4'>
             <QRCodeModal qrcode={qrcode} />
             <Badge
-              variant="default"
-              className="border-none !bg-black/50 p-2 text-white"
+              variant='default'
+              className='border-none !bg-black/50 p-2 text-white'
             >
-              <GrArticle className="mr-2 size-4 text-white" />
+              <GrArticle className='mr-2 size-4 text-white' />
               {review.totalposts}
             </Badge>
           </div>
         </div>
-        <div className="relative w-full max-w-[100%]">
+        <div className='relative w-full max-w-[100%]'>
           <TagSlider tags={tags} />
         </div>
-        <div className="relative">
+        <div className='relative'>
           <div
             className={`absolute right-0 top-3 size-8 rounded-full bg-[url('/images/${countrycode}.svg')] bg-cover bg-center bg-no-repeat`}
           ></div>
         </div>
-        <div className="font-satoshi">
-          <div className="line-clamp-1 text-xl font-semibold text-secondary-foreground">
+        <div className='font-satoshi'>
+          <div className='line-clamp-1 text-xl font-semibold text-secondary-foreground'>
             @{username}
           </div>
-          <div className="line-clamp-3 text-sm text-muted-foreground">
+          <div className='line-clamp-3 text-sm text-muted-foreground'>
             {bio}
           </div>
         </div>
-        <div className="mt-auto">
-          <div className="flex items-center justify-start gap-2 text-muted-foreground">
-            <BsCalendarDateFill className="size-5 text-black dark:text-white" />
-            This month:{" "}
+        <div className='mt-auto'>
+          <div className='flex items-center justify-start gap-2 text-muted-foreground'>
+            <BsCalendarDateFill className='size-5 text-black dark:text-white' />
+            This month:{' '}
             {`${review.monthtime.hour}h : ${review.monthtime.minutes}m`}
           </div>
-          <div className="flex items-center justify-start gap-2 text-muted-foreground">
-            <BiSolidCalendar className="size-5 text-black dark:text-white" />
-            Total time:{" "}
+          <div className='flex items-center justify-start gap-2 text-muted-foreground'>
+            <BiSolidCalendar className='size-5 text-black dark:text-white' />
+            Total time:{' '}
             {`${review.totaltime.hour}h : ${review.totaltime.minutes}m`}
           </div>
         </div>
-        <div className="flex gap-3">
+        <div className='flex gap-3'>
           <CityCard cities={cities} />
           <CategoryCard categories={categories} />
         </div>
-        <div className="flex justify-between">
-          <Button variant="default" className="w-full font-roboto" asChild>
-            <Link href="/profile/[username]" as={`/profile/my-profile`}>
+        <div className='flex justify-between'>
+          <Button variant='default' className='w-full font-roboto' asChild>
+            <Link href='/profile/[username]' as={`/profile/my-profile`}>
               View Detail
             </Link>
           </Button>
         </div>
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export { ProfileCard }
+export { ProfileCard };
