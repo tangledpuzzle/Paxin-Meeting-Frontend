38a3debb6bd66242fe437c838d7c8cfc54637c71
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-29 22:17:20 +0800

    patch: package lock files

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'proxy.paxintrade.com'
-      }
+        hostname: 'proxy.paxintrade.com',
+      },
     ],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://go.paxintrade.com/api/:path*', // Proxy to Backend
-  //     },
-  //   ];
-  // },
   i18n,
   compiler: {
     styledComponents: true,
