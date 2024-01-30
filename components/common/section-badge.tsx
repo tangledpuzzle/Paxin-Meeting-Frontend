e3c840a0411e861c1ac9673910c336f63b8710d7
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-29 22:38:22 -0400

    fix carusel 2

--- components/common/section-badge.tsx
+++ components/common/section-badge.tsx
@@ -11,7 +11,7 @@ function SectionBadge({ children }: SectionBadgeProps) {
   return (
     <Badge
       variant='outline'
-      className='border-gradient mb-[12px] rounded-full p-[13px] dark:border-none'
+      className='border-gradient mb-[12px] rounded-full p-[13px] dark:border-none max-w-fit	'
     >
       <Icons.star className='mr-2 size-5' />
       {children}
