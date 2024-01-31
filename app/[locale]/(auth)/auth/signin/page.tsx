ca3ce851f2ab20f25d8424702136fe5486873a39
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-01 06:24:17 +0900

    fix: add logo into signin, signup pages

--- app/[locale]/(auth)/auth/signin/page.tsx
+++ app/[locale]/(auth)/auth/signin/page.tsx
@@ -2,9 +2,9 @@ import { SignInCard } from '@/components/auth/signin-card';
 
 export default function SigninPage() {
   return (
-    <section className='flex flex-col items-center lg:flex-row'>
-      <div className="flex h-[500px] max-h-screen w-full items-center justify-center bg-[url('/images/auth/signin-panel.png')] bg-cover bg-center bg-no-repeat lg:h-screen lg:w-1/2 xl:w-[60%]"></div>
-      <div className='w-full px-3 py-24 lg:w-1/2 xl:w-[40%]'>
+    <section className='flex size-full flex-col items-center lg:flex-row'>
+      <div className="flex h-0 max-h-screen w-full items-center justify-center bg-[url('/images/auth/signin-panel.png')] bg-cover bg-center bg-no-repeat lg:h-screen lg:w-1/2 xl:w-[60%]"></div>
+      <div className='relative h-screen px-3 py-24 lg:w-1/2 xl:w-[40%]'>
         <SignInCard></SignInCard>
       </div>
     </section>
