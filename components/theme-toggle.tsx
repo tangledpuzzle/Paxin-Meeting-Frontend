6925d5666c0aecd5351b24641d5b6727fd7b8abc
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-02 04:09:22 +0900

    fix: implement ssr in header

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
@@ -8,13 +7,7 @@ import { Button } from '@/components/ui/button';
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
   return (
     <Button
       variant='outline'
