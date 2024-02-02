598a49e3f694a1c3caaebe5d7f5715f5fef97c6a
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-03 02:13:30 +0800

    feat: ci-cd

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'paxintrade-frontend',
+      exec_mode: 'cluster',
+      instances: 2, // 'max' or a number of instances
+      autorestart: true,
+      script: 'node_modules/.bin/next',
+      args: 'start -p 4000',
+    }
+  ]
+}
