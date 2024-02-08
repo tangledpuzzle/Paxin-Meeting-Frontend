0e82a2c70040f1dd9307447354b5d55ca4c0b720
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-08 20:53:09 +0900

    fix: overflow issue in slideshow component(home)

--- components/ui/slideshow.tsx
+++ components/ui/slideshow.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { RiArrowRightSLine } from "react-icons/ri";
-import { RiArrowLeftSLine } from "react-icons/ri";
+import { RiArrowRightSLine } from 'react-icons/ri';
+import { RiArrowLeftSLine } from 'react-icons/ri';
 
 interface SlideshowProps {
   images: string[];
@@ -16,7 +16,7 @@ const Slideshow: React.FC<SlideshowProps> = ({ images, interval }) => {
 
     if (currentIndex < images.length) {
       let progress = 0;
-      const step = 1; 
+      const step = 1;
 
       progressInterval = setInterval(() => {
         progress += step;
@@ -28,10 +28,10 @@ const Slideshow: React.FC<SlideshowProps> = ({ images, interval }) => {
             setCurrentIndex((prevIndex) =>
               prevIndex === images.length - 1 ? 0 : prevIndex + 1
             );
-            setProgressWidth(0); 
-          }, 400); 
+            setProgressWidth(0);
+          }, 400);
         }
-      }, interval / 100); 
+      }, interval / 100);
     }
 
     return () => clearInterval(progressInterval);
@@ -41,18 +41,18 @@ const Slideshow: React.FC<SlideshowProps> = ({ images, interval }) => {
     setCurrentIndex((prevIndex) =>
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
-    setProgressWidth(0); 
+    setProgressWidth(0);
   };
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
-    setProgressWidth(0); 
+    setProgressWidth(0);
   };
 
   return (
-    <div className="slideshow-container relative slider-h top-4">
+    <div className='slideshow-container slider-h relative top-4 overflow-hidden'>
       {images.map((image, index) => (
         <div
           key={index}
@@ -62,19 +62,22 @@ const Slideshow: React.FC<SlideshowProps> = ({ images, interval }) => {
           <img src={image} alt={`Slide ${index + 1}`} className='rounded-md' />
         </div>
       ))}
-      <div className='absolute flex justify-between w-full px-4 items-center h-full'>
-        <div className='text-[24px] bg-green-600/80 rounded-md cursor-pointer' onClick={prevSlide}>
+      <div className='absolute flex h-full w-full items-center justify-between px-4'>
+        <div
+          className='cursor-pointer rounded-md bg-green-600/80 text-[24px]'
+          onClick={prevSlide}
+        >
           <RiArrowLeftSLine />
         </div>
-        <div className='text-[24px] bg-green-600/80 rounded-md cursor-pointer' onClick={nextSlide}>
+        <div
+          className='cursor-pointer rounded-md bg-green-600/80 text-[24px]'
+          onClick={nextSlide}
+        >
           <RiArrowRightSLine />
         </div>
       </div>
-      <div className="progress-bar">
-        <div
-          className="progress"
-          style={{ width: `${progressWidth}%` }}
-        ></div>
+      <div className='progress-bar'>
+        <div className='progress' style={{ width: `${progressWidth}%` }}></div>
       </div>
     </div>
   );
