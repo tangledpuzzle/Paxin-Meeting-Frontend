9d4b1fac434a3982aea704d15e6a69de89feca97
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-17 16:30:49 +0800

    fix: configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,10 @@ module.exports = {
       instances: 2, // 'max' or a number of instances
       autorestart: true,
       script: '.next/standalone/server.js',
+      env: {
+        PORT: 3000,
+        HOSTNAME: 'localhost',
+      },
     },
   ],
 };
