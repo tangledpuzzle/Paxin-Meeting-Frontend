4a694c308a8cc76990767d113397f1eb6e7fbb65
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-20 15:34:06 +0800

    fix: ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,13 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 2, // 'max' or a number of instances
       autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      restart_delay: 5000,
+      max_restarts: 10,
+      min_uptime: "10s",
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 3000',
-      env: {
-        PORT: 3000,
-        HOSTNAME: 'localhost',
-      },
     },
   ],
 };
