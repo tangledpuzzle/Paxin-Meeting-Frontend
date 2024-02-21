2464872ca0c1ef268df7fcd252201dcaa238e718
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-21 10:45:49 -0400

    return get data info data 2

--- app/[locale]/(public)/auto-join-meet/page.tsx
+++ app/[locale]/(public)/auto-join-meet/page.tsx
@@ -1,11 +1,12 @@
-import { unstable_setRequestLocale } from 'next-intl/server';
 import { getServerSession } from 'next-auth';
-import authOptions from '@/lib/authOptions';
+import { unstable_setRequestLocale } from 'next-intl/server';
 import AutoJoinConference from '@/components/profiles/conference/public-meet-auto-join';
-
+import authOptions from '@/lib/authOptions';
+import { generateRandomString, hashTimestamp } from '@/lib/utils';
 
 async function getData(locale: string) {
   const session = await getServerSession(authOptions);
+
   try {
     const res = await fetch(
       `${process.env.API_URL}/api/users/me?language=${locale}`,
@@ -15,31 +16,16 @@ async function getData(locale: string) {
         },
       }
     );
+
     if (!res.ok) {
       return null;
     }
+
     return res.json();
   } catch (error) {
     return null;
   }
 }
-
-function generateRandomString(length: number): string {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      result += charset[randomIndex];
-    }
-  return result;
-}
-
-function hashTimestamp(timestamp: number): string {
-  const hash = require('crypto').createHash('md5');
-  hash.update(timestamp.toString());
-  return hash.digest('hex');
-}
-
 
 export default async function ConferencePage({
   params,
@@ -47,27 +33,17 @@ export default async function ConferencePage({
   params: { locale: string };
 }) {
   unstable_setRequestLocale(params.locale);
-  let userId, userName, userEmail;
+  const {
+    data: {
+      user: { email, id, name },
+    },
+  } = await getData(params.locale);
 
-  const userData = await getData(params.locale);
-
-  if (userData && userData.data && userData.data.user) {
-    const { email, id, name } = userData.data.user;
-    userId = id;
-    userName = name;
-    userEmail = email;
-  } else {
-    const randomPart = generateRandomString(4);
-    const timestampHash = hashTimestamp(Date.now());
-    userId = `user-${randomPart}-${timestampHash}`;
-    userName = `User ${randomPart}`;
-    userEmail = `${randomPart}-${timestampHash}@test.me`;
-  }
-  return (
-    <AutoJoinConference
-      email={userEmail}
-      userId={userId}
-      name={userName}
-    />
-  );
+  const randomPart = generateRandomString(4);
+  const timestampHash = hashTimestamp(Date.now());
+  const userId = `user-${randomPart}-${timestampHash}`;
+  const userName = `User ${randomPart}`;
+  const userEmail = `${randomPart}-${timestampHash}@test.me`;
+  
+  return <AutoJoinConference email={email ?? userEmail} userId={id ?? userId} name={name ?? userName} />;
 }
