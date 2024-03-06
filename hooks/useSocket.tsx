490014ccc90ca7fbdf47ae026c5d68c6bb23a6ee
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-03-07 04:28:16 +0900

    fix : add useSocket to flow of landing page

---
+++ hooks/useSocket.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useSocket = (locale: string): WebSocket | null => {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        const wsProtocol = 'wss:';
+        const wsPath = process.env.NEXT_PUBLIC_SOCKET_URL!;
+        const newSocket = new WebSocket(
+            `${wsProtocol}//${wsPath}/stream/live?langue=` + locale
+        );
+
+        newSocket.onopen = () => {
+            const message = {
+                messageType: 'getADS',
+                locale: locale,
+            };
+            newSocket.send(JSON.stringify(message));
+        };
+
+        newSocket.onmessage = (event) => { };
+
+        setSocket(newSocket);
+
+        return () => {
+            newSocket.close();
+        };
+    }, [locale]);
+
+    return socket;
+};
+
+export default useSocket
