39c927197cd5e181c8938f9f689ff80723f3761c
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-03-07 18:16:15 -0400

    fix styles

--- components/home/flow/index.tsx
+++ components/home/flow/index.tsx
@@ -96,10 +96,10 @@ export default function FlowSection() {
   return (
     <div className='w-full'>
       {maxPage > 1 && (
-        <div className='flex  gap-1 z-20 w-[100px]  fixed md:sticky bottom-0 h-[35px] top-[calc(100vh_-_3rem)] md:top-[110px] md:right-[50px] !left-4 md:left-[calc(100%_-_10rem)] md:-mt-[152px]'>
+        <div className='flex  gap-1 z-20 w-[100px]  fixed md:sticky bottom-0 h-[35px] top-[calc(100dvh_-_3.6rem)] md:top-[110px] md:right-[50px] !left-4 md:left-[calc(100%_-_10rem)] md:-mt-[152px]'>
         <Button
             aria-disabled={Number(searchParams.get('page') || 1) === 1}
-            className='w-[30px] p-0 aria-[disabled=true]:cursor-not-allowed aria-[disabled=true]:opacity-60  md:w-[50px]'
+            className='w-[40px] p-0 aria-[disabled=true]:cursor-not-allowed aria-[disabled=true]:opacity-60  md:w-[50px]'
             asChild
           >
             <Link href={prevPageLink || ''}>
@@ -109,7 +109,7 @@ export default function FlowSection() {
           </Button>
           <Button
             aria-disabled={Number(searchParams.get('page') || 1) === maxPage}
-            className='w-[30px] p-0 aria-[disabled=true]:cursor-not-allowed aria-[disabled=true]:opacity-60 md:w-[50px]'
+            className='w-[40px] p-0 aria-[disabled=true]:cursor-not-allowed aria-[disabled=true]:opacity-60 md:w-[50px]'
             asChild
           >
             <Link href={nextPageLink || ''}>
