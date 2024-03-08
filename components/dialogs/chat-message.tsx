6c293f7cd6fcd30d5d305799b88fb46d368da0b0
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-03-09 06:08:54 +0900

    fix: add classname for markdown

--- components/dialogs/chat-message.tsx
+++ components/dialogs/chat-message.tsx
@@ -80,7 +80,7 @@ export default function ChatMessage(props: ChatMessageProps) {
                   <span className='select-none'>{t('message_deleted')}</span>
                 </>
               ) : (
-                <ReactMarkdown children={props.message} />
+                <ReactMarkdown className='prose' children={props.message} />
               )}
             </div>
           </div>
