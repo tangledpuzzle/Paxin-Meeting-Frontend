6cff1dcbf8ca3534e45c664aa5eb716b9cd9d056
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-03-14 00:33:27 +0900

    fix: client.header.tsx

--- components/header/client.header.tsx
+++ components/header/client.header.tsx
@@ -25,7 +25,7 @@ export default function ClientHeader({ data }: ClientHeaderProps) {
       <div className='border-gardient-h relative top-[80px] w-full'></div>
       <div className='flex h-20 items-center space-x-4 px-2 sm:justify-between sm:space-x-0 md:px-8'>
         <MainNav items={siteConfig.mainNav} />
-        {data && <AlarmNav />}
+        {data && <AlarmNav authenticated={!!data}/>}
         <RightNav
           user={
             data
