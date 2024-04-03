44c8e4f5049639dd97bbd1a4a86e60eb764acf69
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-04-03 15:40:40 +0000

    fix: adjust wrong noise filter integration

--- app/[locale]/(public)/meet/layout.tsx
+++ app/[locale]/(public)/meet/layout.tsx
@@ -1,14 +1,4 @@
-'use client';
-
-import {
-  Room,
-  Track,
-  RoomEvent,
-  LocalAudioTrack,
-  TrackProcessor,
-} from 'livekit-client';
-import { ReactNode, useEffect, useState } from 'react';
-// import MeetHeader from '@/components/meet/newHeader';
+import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
@@ -19,52 +9,6 @@ export default function ProfilePageLayout({
   children,
   params: { locale },
 }: Props) {
-  const [room, setRoom] = useState<Room | null>(null);
-
-  useEffect(() => {
-    // This ensures the code runs client-side
-    const initRoomAndFilter = async () => {
-      const room = new Room();
-      // Your room setup code here...
-
-      // Dynamically import the krisp noise filter
-      const { KrispNoiseFilter, isKrispNoiseFilterSupported } = await import(
-        '@livekit/krisp-noise-filter'
-      );
-
-      room.on(RoomEvent.LocalTrackPublished, async (trackPublication) => {
-        if (
-          trackPublication.source === Track.Source.Microphone &&
-          trackPublication.track instanceof LocalAudioTrack
-        ) {
-          if (!isKrispNoiseFilterSupported()) {
-            console.warn(
-              'Enhanced noise filter is currently not supported on this browser'
-            );
-            return;
-          }
-          // Once instantiated the filter will begin initializing and will download additional resources
-          const krispProcessor = KrispNoiseFilter();
-          console.log('Enabling LiveKit enhanced noise filter');
-          await trackPublication.track.setProcessor(krispProcessor as any);
-
-          // Once you want to stop the Krisp processor, simply call
-          // await trackPublication.track.stopProcessor()
-        }
-      });
-
-      setRoom(room);
-    };
-
-    initRoomAndFilter();
-
-    return () => {
-      // Cleanup, e.g., leave room, stop processing, etc.
-      if (room) {
-        room.disconnect();
-      }
-    };
-  }, []);
 
   return <div>{children}</div>;
 }
