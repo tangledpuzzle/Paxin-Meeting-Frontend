9428a5a7a9bfdb6a91dee97a5d26a50cda8838f7
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-05-12 08:44:22 -0400

    fix build ts

--- components/header/smallTrade/small-host.tsx
+++ components/header/smallTrade/small-host.tsx
@@ -3,24 +3,30 @@ import { LiveKitRoom } from '@livekit/components-react';
 import { useSession } from 'next-auth/react';
 import React, { useRef } from 'react';
 
+interface SmallHostProps {
+    token: string; 
+    roomId: string;
+}
+
 export default function SmallHost({
     token, roomId
-}) {
+}: SmallHostProps) { 
     const { data } = useSession();
     const previewVideoEl = useRef<HTMLVideoElement>(null);
-    return (<LiveKitRoom
-        token={token}
-        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className='relative flex h-[calc(100vh-81px)] flex-col'
-    >
-        <div className='aspect-video flex-1 rounded-sm border bg-neutral-200 dark:bg-neutral-800 '>
-            <video
-            ref={previewVideoEl}
-            className='h-full'
-            width='100%'
-            height='100%'
-            />
-        </div>
-    </LiveKitRoom>);
-
+    return (
+        <LiveKitRoom
+            token={token}
+            serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+            className='relative flex h-[calc(100vh-81px)] flex-col'
+        >
+            <div className='aspect-video flex-1 rounded-sm border bg-neutral-200 dark:bg-neutral-800 '>
+                <video
+                    ref={previewVideoEl}
+                    className='h-full'
+                    width='100%'
+                    height='100%'
+                />
+            </div>
+        </LiveKitRoom>
+    );
 }
