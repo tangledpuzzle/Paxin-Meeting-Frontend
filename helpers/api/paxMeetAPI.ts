d8627fd3dcb10941d1f982a7c2c2ec9c1a31991b
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-06-11 12:38:28 +0400

    remove pax meet name from ui 3

--- helpers/api/paxMeetAPI.ts
+++ helpers/api/paxMeetAPI.ts
@@ -118,9 +118,9 @@ export const createRoom = async (
     creator: userId,
     empty_timeout: 60 * 60 * 2,
     metadata: {
-      room_title: 'Pax Real-Time Meeting',
+      room_title: 'Встреча в реальном времени',
       welcome_message:
-        'Welcome to PaxMeet!<br /> To share microphone click mic icon from bottom left side.',
+        'Добро пожаловать в конференцию!<br />.',
       //webhook_url: "http://example.com",
       //logout_url: "http://example.com",
       room_features: {
