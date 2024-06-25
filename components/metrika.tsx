7d02e96a4822c263b8555c56930cc8f98dd1fab8
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-06-25 23:49:14 +0400

    add metrika yandex

---
+++ components/metrika.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+
+export function Metrika() {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    if (window.ym) {
+      window.ym(97671140, "hit", window.location.href);
+    }
+  }, [pathName, searchParams]);
+  return (
+    <Script id="yandex-metrika">
+      {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(97671140, "init", {
+          defer: true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        });    
+      `}
+    </Script>
+  );
+}
