bddb0efa40dda3b6d056cdc979ca519416703f1c
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-06-25 18:20:19 +0400

    added progress bar2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -78,20 +78,20 @@ export default async function RootLayout({
         )}
       >
         <NextTopLoader
-  color="#2299DD"
-  initialPosition={0.08}
-  crawlSpeed={200}
-  height={3}
-  crawl={true}
-  showSpinner={true}
-  easing="ease"
-  speed={200}
-  shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-  template='<div class="bar" role="bar"><div class="peg"></div></div> 
-  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-  zIndex={1600}
-  showAtBottom={false}
-/>
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+          <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <SessionProviders session={session}>
           <Providers>
             <StoreProvider>
