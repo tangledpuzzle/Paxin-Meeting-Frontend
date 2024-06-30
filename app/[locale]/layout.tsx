828e6e959eccac958e0b4abd1c593c93dc2c1361
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-06-30 16:35:40 +0400

    add donat func

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -84,7 +84,7 @@ export default async function RootLayout({
           crawlSpeed={200}
           height={3}
           crawl={true}
-          showSpinner={true}
+          showSpinner={false}
           easing="ease"
           speed={200}
           shadow="0 0 10px #2299DD,0 0 5px #2299DD"
